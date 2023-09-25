5bcb0866712933d770f88489e1b9832ff8f58b4b
M Yoga Atmajaya <myogaatmajaya@gmail.com>
2023-09-25 14:46:11 +0700

    ORM

--- interface/Provinsies.ts
+++ interface/Provinsies.ts
@@ -1,4 +1,4 @@
 export default interface ProvinsiIt {
     id: number
-    title: string
+    name: string
   }
--- src/mocks/Provinsi.ts
+++ src/mocks/Provinsi.ts
@@ -2,17 +2,53 @@ import Provinsies from "../../interface/Provinsies";
 
 const Provinsi: Provinsies[] = [
   {
-    id: 1,
-    title: "Jawa barat"
+    "id": 11,
+    "name": "ACEH"
   },
   {
-    id: 2,
-    title: "Jawa Timur"
+    "id": 12,
+    "name": "SUMATERA UTARA"
   },
   {
-    id: 3,
-    title: "Jawa Tengah)"
-  }
+    "id": 13,
+    "name": "SUMATERA BARAT"
+  },
+  {
+    "id": 14,
+    "name": "RIAU"
+  },
+  {
+    "id": 15,
+    "name": "JAMBI"
+  },
+  {
+    "id": 16,
+    "name": "SUMATERA SELATAN"
+  },
+  {
+    "id": 17,
+    "name": "BENGKULU"
+  },
+  {
+    "id": 18,
+    "name": "LAMPUNG"
+  },
+  {
+    "id": 19,
+    "name": "KEPULAUAN BANGKA BELITUNG"
+  },
+  {
+    "id": 21,
+    "name": "KEPULAUAN RIAU",
+  },
+  {
+    "id": 31,
+    "name": "DKI JAKARTA"
+  },
+  {
+    "id": 32,
+    "name": "JAWA BARAT"
+  },
 ]
 
 export default Provinsi;
